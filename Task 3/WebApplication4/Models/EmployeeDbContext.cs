cc75b3789f7f0cd140e83223839024ab4ee27af8
abhi7083 <71085654+abhi7083@users.noreply.github.com>
2022-08-03 19:25:58 +0530

    Add files via upload

---
+++ Task 3/WebApplication4/Models/EmployeeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+		public DbSet<employee> employees { get; set; }
+
+		public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+		 : base(options)
+		{
+
+		}
+
+	}
+}
---
+++ Task 3/WebApplication4/Models/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication4.Models
+{
+    public interface IEmployee
+    {
+        public List<employee> getAllEmployees();
+
+        public employee getEmployeeById(int id);
+
+        public employee createEmployee(employee obj);
+        public employee updateEmployee(employee obj);
+
+        public employee deleteEmployee(int id);
+    }
+}
